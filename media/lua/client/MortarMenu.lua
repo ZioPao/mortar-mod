7333290d9a8cdb3479eec8313740d6b64dd5d2e7
Pao <picchetti1998@gmail.com>
2023-02-18 23:00:26 +0100

    Fixes to context menu

--- media/lua/client/MortarMenu.lua
+++ media/lua/client/MortarMenu.lua
@@ -1,6 +1,7 @@
 
+local spotter_table = {}
 
-local SetSpotter = function(sq, player)
+local SetSpotter = function(_, player)
     print("Mortar: Setting spotter")
 
     Mortar.spotter = player
@@ -9,52 +10,89 @@ local SetSpotter = function(sq, player)
 end
 
 
-local SearchForSpotter = function(operator_player)
+local CheckSpotterForWalkieTalkie = function(spotter)
+
+    local accepted_item = "Radio.WalkieTalkie"
+
+
+    local primary_hand_item = spotter:getPrimaryHandItem()
+    local secondary_hand_item = spotter:getSecondaryHandItem()
+
+
+    if primary_hand_item then
+        local primary_item_type = primary_hand_item:getFullType()
+
+        if string.find(primary_item_type,accepted_item) then
+            return true
+        end
+
+    elseif secondary_hand_item then
+        local secondary_item_type = secondary_hand_item:getFullType()
+
+        if string.find(secondary_item_type, accepted_item) then
+            return true
+        end
+    end
+
+    return false
+
+
+end
+
+
+local SearchForSpotter = function(mortar_menu, world_obj)
     -- TODO Search the cell for a player with something on him like a radio
     print("Mortar: Searching spotter")
+    for x = world_obj:getSquare():getX() - 1, world_obj:getSquare():getX() + 1 do
+        for y = world_obj:getSquare():getY() - 1, world_obj:getSquare():getY() + 1 do
+            local sq = getCell():getGridSquare(x,y,world_obj:getSquare():getZ())
 
-    -- TODO search player in zone
+            if sq then
+                for i = 0, sq:getMovingObjects():size() - 1 do
+                    local obj = sq:getMovingObjects():get(i)
 
+                    if instanceof(obj, "IsoPlayer") then
 
+                        if spotter_table[obj:getUsername()] == nil then
+                            spotter_table[obj:getUsername()] = true
 
-    -- TODO Check if the player is holding a radio item
-    return getPlayer()
+                            print("Checking player for spotter")
+                            local walkietalkie_check = CheckSpotterForWalkieTalkie(obj)
+                            if walkietalkie_check then
+                                mortar_menu:addOption(getText("UI_ContextMenu_Spotter") .. obj:getUsername(), _, SetSpotter, obj)
+                            end
 
+                        end
+                    end
+                end
+            end
+        end
+    end
 end
 
 
 
 
 local CreateMortarContextMenu = function(player, context, world_objects, _)
-    local player_obj = getSpecificPlayer(player)
+
+    --local player_obj = getSpecificPlayer(player)
+    local mortar_menu
+    local root_menu
+    spotter_table = {}        -- Reset
 
     for _, v in pairs(world_objects) do
         -- TODO Check if v is a mortar
         if instanceof(v, "IsoObject") then      -- TODO This should check if it's the mortar
-            local mortar_menu = context:getNew(context)
-            local root_menu = context:addOption(getText("UI_ContextMenu_OperateMortar"), world_objects, nil)
-            context:addSubMenu(root_menu, mortar_menu)
 
-
-            for x = v:getSquare():getX() - 1, v:getSquare():getX() + 1 do
-                for y = v:getSquare():getY() - 1, v:getSquare():getY() + 1 do
-                    local sq = getCell():getGridSquare(x,y,v:getSquare():getZ())
-
-                    if sq then
-                        for i = 0, sq:getMovingObjects():size() - 1 do
-                            local obj = sq:getMovingObjects():get(i)
-
-                            if instanceof(obj, "IsoPlayer") then
-                                print("Checking player for spotter")
-                                mortar_menu:addOption(getText("UI_ContextMenu_Spotter") .. obj:getUsername(), world_objects, SetSpotter, obj)
-
-                            end
-
-                        end
-                    end
-
-                end
+            if mortar_menu == nil then
+                mortar_menu = context:getNew(context)
+                root_menu = context:addOption(getText("UI_ContextMenu_OperateMortar"), world_objects, nil)
             end
+
+            context:addSubMenu(root_menu, mortar_menu)
+            SearchForSpotter(mortar_menu, v)
+
+
 
 
             -- TODO add the options such as
