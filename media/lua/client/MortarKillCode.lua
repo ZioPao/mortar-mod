a889b90ab6c71487b27b8e6c9feacc4db4a888d4
Glytch3r <105521152+Glytch3r@users.noreply.github.com>
2023-02-19 09:20:51 +0800

    add mortar killcode

--- media/lua/client/MortarKillCode.lua
+++ media/lua/client/MortarKillCode.lua
@@ -21,132 +21,32 @@ Discord: Glytch3r#2892
 ----------------------------------------------------------------------------------------------------------------------------
 --]]
 
-Mortar = {}
-Mortar.directions = {
-    ["N"] = {0, -1},
-    ["NE"] = {math.sqrt(2) / 2, -math.sqrt(2) / 2},
-    ["E"] = {1, 0},
-    ["SE"] = {math.sqrt(2) / 2, math.sqrt(2) / 2},
-    ["S"] = {0, 1},
-    ["SW"] = {-math.sqrt(2) / 2, math.sqrt(2) / 2},
-    ["W"] = {-1, 0},
-    ["NW"] = {-math.sqrt(2) / 2, -math.sqrt(2) / 2}
-}
-Mortar.distMin = 4
-Mortar.distMax = 12
-Mortar.distSteps = 2
-Mortar.rad = 5
+MortarKill = {}
 
+function MortarKill.killzed(zed) 
+	if not zed then return end
+	local square = zed:getSquare() 
+	for i=1,square:getObjects():size() do
+		local object = square:getObjects():get(i-1)
+		if instanceof(object, "IsoFire") and not object:isPermanent() and square:getModData()['mortarHit']  then
+			if instanceof(zombie, "IsoZombie") then
+				zed:Kill(zed); break		
+			end
+		end
+	end
+end 
+Events.OnZombieUpdate.Add(MortarKill.killzed)
 
-
-
-
-
-function Mortar.init()
-
-    print("Mortar: Initializing")
-
-
-    local pl = getPlayer()
-    if not pl:getModData()['mortarDistance'] then
-        pl:getModData()['mortarDistance'] = 8
-    end
-
-
-
-
-
-
-end
-Events.OnGameStart.Add(Mortar.init)
-
-function Mortar.debris(square)
-    local dug = IsoObject.new(square, "mortar_" .. ZombRand(64), "", false)
-    square:AddTileObject(dug)
-    if isClient() then
-        dug:transmitCompleteItemToServer();
-    end
-    ISInventoryPage.renderDirty = true;
-end
-function Mortar.roll(chance)
-    local roll = ZombRand(1, 101);
-    if roll <= chance then
-        return true
-    end
-end
-function Mortar.groundZero(bommX, bommY, bommZ, radius)
-    local cell = getCell()
-    local playerObj = getSpecificPlayer(0)
-    playerObj:startMuzzleFlash()
-    for x = bommX - radius, bommX + radius + 1 do
-        for y = bommY - radius, bommY + radius + 1 do
-            if IsoUtils.DistanceTo(bommX, bommY, x + 0.5, y + 0.5) <= radius then
-                local sq = cell:getGridSquare(x, y, bommZ)
-                local Xtype = 'addFireOnSquare'
-                if Mortar.roll(20) then
-                    Xtype = 'addSmokeOnSquare'
-                end
-                if sq:Is(IsoFlagType.burning) then 
-                    sq:getModData()['mortarHit'] = true 
-                    Events.OnTick.Add(function() 
-                        if sq:getModData()['mortarHit'] and not sq:Is(IsoFlagType.burning)  then 
-                            sq:getModData()['mortarHit'] = nil
-                        end
-                    end) 
-                end
-                if Mortar.roll(60) then
-                    local args = {
-                        x = x,
-                        y = y,
-                        z = bommZ
-                    }
-                    sendClientCommand(playerObj, 'object', Xtype, args)
-                end
-                chance = 40
-                if Mortar.roll(chance) then
-                    Mortar.debris(sq)
-                end
-            end
-        end
-    end
-end
-
-Mortar.ExecuteFire = function(operator, rad, dist)
-    local nx = Mortar.directions[tostring(Mortar.spotter:getDir())][1];
-    local ny = Mortar.directions[tostring(Mortar.spotter:getDir())][2]
-    local bommX = math.floor(operator:getX() + (nx * dist))
-    local bommY = math.floor(operator:getY() + (ny * dist))
-    local bommZ = operator:getZ()
-    -- TODO add a checker and setter for z trajectory based on the highest floor available
-    local trajectory = getCell():getGridSquare(bommX, bommY, bommZ)
-    local Xtype = 'addExplosionOnSquare'
-    local finalRad = ZombRand(3, rad)
-    Mortar.groundZero(bommX, bommY, bommZ, finalRad)
-end
-
-
-Mortar.StartFiring = function(rad, dist)
-    local pl = getPlayer()
-    print("Mortar: Trying to fire")
-    -- SP or when there's no need for a spotter
-    if not isServer() and not isClient() or not SandboxVars.Mortar.NecessarySpotter then
-        print("SP or no Necessary Spotter")
-        Mortar.spotter = pl
-        Mortar.ExecuteFire(pl, rad, dist)
-    else
-        if Mortar.spotter == nil then
-            pl:Say("I don't have a spotter right now")
-            return
-        end
-        if Mortar.CheckSpotterForWalkieTalkie(Mortar.spotter) then
-            -- TODO Add a check for visibility
-            Mortar.ExecuteFire(pl, rad, dist)
-        else
-            pl:Say("My spotter has no Walkie Talkie on their hand")
-            Mortar.spotter = nil
-        end
-
-    end
-
-
-end
+function MortarKill.killplayer(pl) 
+	if not pl then return end
+	local square = pl:getSquare() 
+	for i=1,square:getObjects():size() do
+		local object = square:getObjects():get(i-1)
+		if instanceof(object, "IsoFire") and not object:isPermanent() and square:getModData()['mortarHit'] then
+			if instanceof(pl, "IsoGameCharacter") and pl:isOnFire() then
+				pl:Kill(pl); break
+			end
+		end
+	end
+end 
+Events.OnPlayerUpdate.Add(MortarKill.killplayer)
