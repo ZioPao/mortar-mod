3a53a2368fa4bc110b123cd8eaa97533a1802bca
Glytch3r <105521152+Glytch3r@users.noreply.github.com>
2023-02-22 19:38:16 +0800

    improved mortar round checker

--- media/lua/client/MortarReloadHandler.lua
+++ media/lua/client/MortarReloadHandler.lua
@@ -1,8 +1,15 @@
 
  
 function mortarReloader(item)  -- TODO checks if bomber
-    if item:getFullType() == "Mortar.MortarRound" then 
-        getPlayer():playEmote("_mortarReload") 
+    local inv = getPlayer():getInventory()      
+    --if item:getFullType() == "Mortar.MortarRound" then 
+   if inv:getItemCountRecurse('Mortar.MortarRound') then
+        getPlayer():playEmote("_mortarReload")         
+        inv:getItemCountRecurse('Mortar.MortarRound');
+        inv:DoRemoveItem(item);
+        return true
+    else
+        getPlayer():Say(tostring('I have no ammo')) 
         return true
     end
 end 
