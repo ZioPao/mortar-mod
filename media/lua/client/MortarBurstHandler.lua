b5fbba95d78506045bb7c6e16a92380b30273e31
Glytch3r <105521152+Glytch3r@users.noreply.github.com>
2023-02-24 12:53:08 +0800

    merged

---
+++ media/lua/client/MortarBurstHandler.lua
@@ -0,0 +1,76 @@
+
+
+
+MortartBurstTiles = {
+"mortarburst_0",
+"mortarburst_1",
+"mortarburst_2",
+"mortarburst_3",
+"mortarburst_4",
+"mortarburst_5",
+"mortarburst_6",
+"mortarburst_7",
+"mortarburst_8",
+"mortarburst_9",
+"mortarburst_10",
+"mortarburst_11",
+"mortarburst_12",
+"mortarburst_13",
+"mortarburst_14"
+}
+--[[ for i = 1, #MortartBurstTiles do 
+    print(MortartBurstTiles[i])
+end ]]
+
+local function destroyTile(obj)
+	if isClient() then
+		sledgeDestroy(obj)
+	else
+		obj:getSquare():transmitRemoveItemFromSquare(obj)
+	end
+end
+
+function BurstTicker(mortar, count ,ticks, square)
+
+	ticks = ticks - 1
+	if ticks <= 0 then 
+	ticks = 12 
+	count = count + 1
+	end
+
+	if ticks % 3 then
+		if count == 14 then
+			destroyTile(mortar)
+			   -- the tile starts at 0 and ends 64 -8 (8  mortar tiles)
+		local dug = IsoObject.new(square, "mortar_" .. ZombRand(63)-8, "", false) 
+		square:AddTileObject(dug)
+		if isClient() then
+			dug:transmitCompleteItemToServer()
+		end
+		ISInventoryPage.dirtyUI()
+
+			Events.OnTick.Remove(BurstTicker) 
+		end
+
+
+		local newtile = MortartBurstTiles[tostring(count)]
+			mortar:setSprite(newtile)
+			mortar:getSprite():setName(newtile)
+
+			mortar:transmitUpdatedSpriteToServer()
+			mortar:transmitUpdatedSpriteToClients()
+			--mortar:transmitCompleteItemToServer()
+			getPlayerLoot(0):refreshBackpacks()
+			ISInventoryPage.dirtyUI()
+
+		count = count + 1
+		getPlayer():setHaloNote(tostring(ticks),100,100,200,100) --debug only delete later
+	end
+end
+
+
+--[[ 
+
+Events.OnTick.Remove(BurstTicker)
+Events.OnTick.Add(BurstTicker)
+]]
